1da8bede75073fcac650b733f027dc6dcaa75514
Onesuch <experts1994@mail.ru>
2016-06-22 12:56:43 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var reg = /[A-Z]/gi;
+var arrOne = [];
+var arrTwo = [];
 var allElements = {
     writeTextAtInputOne : $('.writeTextAtInputOne'),
     multiplication : $('.multiplication'),
@@ -8,26 +9,70 @@ var allElements = {
     writeTextAtInputTwo : $('.writeTextAtInputTwo'),
     answer : $('.answer')
 };
+var allRomanNumbers = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
 
 function logicalExpressionRendering(expressionRendering) {
-    var result;
-    if (expressionRendering === 'X') {
-        result = allElements.writeTextAtInputOne * allElements.writeTextAtInputTwo;
-    }
-    if (expressionRendering === '/') {
-        result = allElements.writeTextAtInputOne / allElements.writeTextAtInputTwo;
-    }
-    if (expressionRendering === '-') {
-        result = allElements.writeTextAtInputOne - allElements.writeTextAtInputTwo;
-    }
-    if (expressionRendering === '+') {
-        result = allElements.writeTextAtInputOne + allElements.writeTextAtInputTwo;
-    }
-    textInputValidation(result);
-}
-
-function textInputValidation(result) {
     var writeTextForInputOne = allElements.writeTextAtInputOne.val();
     var writeTextForInputTwo = allElements.writeTextAtInputTwo.val();
-};
+    var result;
+    if (expressionRendering === 'X') {
+        result = writeTextForInputOne * writeTextForInputTwo;
+        console.log('Выполнено умножение');
+    }
+    if (expressionRendering === '/') {
+        result = writeTextForInputOne / writeTextForInputTwo;
+        console.log('Выполнено деление');
+    }
+    if (expressionRendering === '-') {
+        result = writeTextForInputOne - writeTextForInputTwo;
+        console.log('Выполнено вычитание');
+    }
+    if (expressionRendering === '+') {
+        result = writeTextForInputOne + writeTextForInputTwo;
+        console.log('Выполнено прибавление');
+    }
+    textInputValidation(result,writeTextForInputOne,writeTextForInputTwo);
+}
+
+function textInputValidation(result,writeTextForInputOne,writeTextForInputTwo) {
+    for (var i = 0; i <= result.length; i++) {
+        switch (result[i]) {
+            case 'I':
+                result = 1;
+                break;
+            case 'V':
+                result = 5;
+                break;
+            case 'X':
+                result = 10;
+                break;
+            case 'L':
+                result = 50;
+                break;
+            case 'C':
+                result = 100;
+                break;
+            case 'D':
+                result = 500;
+                break;
+            case 'M':
+                result = 1000;
+                break;
+        }
+        if (writeTextForInputOne) {
+            writeTextForInputOne.push(arrOne);
+        }
+        else {
+            writeTextForInputTwo.push(arrTwo);
+        }
+    }
+}
 
